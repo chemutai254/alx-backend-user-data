fa404d67c3474884fb53b6c93693ed23dd30644a
chemutai254 <nancy.chemutai254>
2022-09-05 11:26:15 -0700

    test file main

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,6 +1,21 @@
 #!/usr/bin/env python3
 """Function that returns the log message obfuscated"""
+import logging
 
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        NotImplementedError
 
 def filter_datum(fields: List[str],
                  redaction: str,
