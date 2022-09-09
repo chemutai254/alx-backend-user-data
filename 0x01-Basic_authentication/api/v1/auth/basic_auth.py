6842d84c26b2b305783db41a5788b84f4e4c735c
chemutai254 <nancy.chemutai254>
2022-09-09 11:23:23 -0700

    Basic - User credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """Basic authentication: class that inherits from auth class"""
-
+from typing import tuple
 from api.v1.auth.auth import Auth
 
 
@@ -29,3 +29,14 @@ class BasicAuth(Auth):
                     .decode('utf-8'))
             except Exception:
                 return None
+
+    def extract_user_credentials(
+                                 self,
+                                 decoded_base64_authorization_header:
+                                 str) -> (str, str):
+        """Returns username and password from base64"""
+        if (decoded_base64_authorization_header is None
+                or type(decoded_base64_authorization_header) is not str
+                or ":" not in decoded_base64_authorization_header):
+            return (None, None)
+        return tuple(decoded_base64_authorization_header.split(":", 1))
