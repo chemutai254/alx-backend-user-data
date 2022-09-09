f1a62f69068055a8daa199b6a74dd87c6dec66c6
chemutai254 <nancy.chemutai254>
2022-09-09 07:07:37 -0700

    Inheritance

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Basic authentication: class that inherits from auth class"""
+
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """Inherits properties of class Auth"""
